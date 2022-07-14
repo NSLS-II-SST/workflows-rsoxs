943ad7b919e5d7b4a466ade125aab7121bdf4938
Garrett Bischof <gbischof@prefect-worker1.nsls2.bnl.gov>
2022-07-14 13:20:08 -0400

    clean up the code

--- export.py
+++ export.py
@@ -6,11 +6,11 @@ import numpy
 from prefect import Flow, Parameter, task
 from tiled.client import from_profile
 
+EXPORT_PATH = Path("/nsls2/data/dssi/scratch/prefect-outputs/rsoxs/")
+
 tiled_client = from_profile("nsls2", username=None)["rsoxs"]
 tiled_client_raw = tiled_client["raw"]
 tiled_client_processed = tiled_client["sandbox"]
-
-EXPORT_PATH = Path("/nsls2/data/dssi/scratch/prefect-outputs/rsoxs/")
 
 
 @task
@@ -22,21 +22,43 @@ def write_dark_subtraction(ref):
     Subtract dark frame images from the data,
     and write the result to tiled.
 
-    This task is typically triggered manually from the Prefect webapp, but eventually
-    it can be triggered by the end_of_run workflow.
-
     Parameters
     ----------
     ref: string
         This is the reference to the BlueskyRun to be exported. It can be
         a partial uid, a full uid, a scan_id, or an index (e.g. -1).
+
+    Returns
+    -------
+    results: dict
+        A dictionary that maps field_name to the matching processed uid.
     """
 
-    # Defining some variable that we will use later in this function.
+    def safe_subtract(light, dark, pedestal=100):
+        """
+        Subtract a dark_frame from a light_frame.
+        Parameters
+        ----------
+        light: array
+            This is the light_frame.
+        dark: array
+            This is the dark_frame.
+        pededtal: integer
+            An offset to avoid negative results.
+        """
+
+        dark.load()
+        light.load()
+
+        dark = dark - pedestal
+
+        dark = numpy.clip(dark, a_min=0, a_max=None)
+
+        return numpy.clip(
+            light - dark.reindex_like(light, "ffill"), a_min=0, a_max=None
+        ).astype(light.dtype)
+
     run = tiled_client_raw[ref]
-    # Note: We need to grab the full uid so we can pair the subtracted
-    # data and the raw data together when we write the subtracted data
-    # into Tiled.
     full_uid = run.start["uid"]
     primary_data = run["primary"]["data"]
     dark_data = run["dark"]["data"]
@@ -56,7 +78,7 @@ def write_dark_subtraction(ref):
     found_fields = export_fields & primary_fields
 
     # Map field to processed uid to use in other tasks.
-    processed_uid_dict = {}
+    results = {}
 
     # Write the dark substracted images to tiled.
     for field in found_fields:
@@ -72,45 +94,35 @@ def write_dark_subtraction(ref):
                 "operation": "dark subtraction",
             },
         )
-        processed_uid_dict[field] = processed_uid
-    return processed_uid_dict
-
-
-def safe_subtract(light, dark, pedestal=100):
-    dark.load()
-    light.load()
-
-    dark = dark - pedestal
-
-    dark = numpy.clip(dark, a_min=0, a_max=None)
-
-    return numpy.clip(
-        light - dark.reindex_like(light, "ffill"), a_min=0, a_max=None
-    ).astype(light.dtype)
+        results[field] = processed_uid
+    return results
 
 
 @task
-def tiff_export(ref, processed_uids):
+def tiff_export(raw_ref, processed_refs):
 
     """
     Export processed data into a tiff file.
 
     Parameters
     ----------
-    ref: string
+    raw_ref: string
         Reference to a BlueskyRun. Can be a full uid, a partial uid,
         a scan id, or an index (e.g. -1).
-
+    processed_refs: dict
+        A dictionary that maps field_name to the matching processed_ref.
     """
-    start = tiled_client_raw[ref].start
+
     # This is the result of combining 2 streams so we'll set the stream name as primary
     # Maybe we shouldn't use a stream name in the filename at all,
     # but for now we are maintaining backward-compatibility with existing names.
     STREAM_NAME = "primary"
 
+    start = tiled_client_raw[raw_ref].start
+
     directory = EXPORT_PATH / "auto" / start["project_name"] / f"{start['scan_id']}"
     directory.mkdir(parents=True, exist_ok=True)
-    for field, processed_uid in processed_uids.items():
+    for field, processed_uid in processed_refs.items():
         dataset = tiled_client_processed[processed_uid]
         assert field == dataset.metadata["field"]
         num_frames = len(dataset)
@@ -123,7 +135,7 @@ def tiff_export(ref, processed_uids):
 
 
 @task
-def csv_export(ref):
+def csv_export(raw_ref):
 
     """
     Export each stream as a CSV file.
@@ -134,67 +146,81 @@ def csv_export(ref):
 
     Parameters
     ----------
-    ref: string
+    raw_ref: string
         Reference to a BlueskyRun. Can be a full uid, a partial uid,
         a scan id, or an index (e.g. -1).
 
     """
-    run = tiled_client_raw[ref]
+    run = tiled_client_raw[raw_ref]
     start = run.start
 
+    # Make the directories.
     base_directory = EXPORT_PATH / "auto" / start["project_name"]
     base_directory.mkdir(parents=True, exist_ok=True)
-    for stream_name, stream in run.items():
-        if stream_name != "primary":
-            directory = base_directory / f"{start['scan_id']}"
-            directory.mkdir(parents=True, exist_ok=True)
-        else:
-            directory = base_directory
+    stream_directory = base_directory / f"{start['scan_id']}"
+    stream_directory.mkdir(parents=True, exist_ok=True)
+
+    def is_scalar(structure, field):
+        """
+        Checks if a field is scalar.
+        """
         # TODO Soon Tiled will support
         # >>> stream["data"].search(ArrayNdim(1))
         # to filter it down to only scalar fields.
         # For now we need to filter on the client side.
-        scalar_fields = []
-        dataset = stream["data"]
-        structure = dataset.structure()
-        for field in dataset:
-            # TODO: seq_num column
-            # WARNING: Future Tiled release is highly likely to break this API
-            # (but also provide the nicer way to do this described above).
-            shape = structure.macro.data_vars[field].macro.variable.macro.shape
-            ndim = len(shape)
-            if ndim == 1:
-                scalar_fields.append(field)
-        if set(scalar_fields) == set(dataset):
-            scalar_fields = None
-        # We need a seq_num column, which the server does not include, so we
-        # do export on the client side.
-        ds = dataset.read(variables=scalar_fields)
-        df = ds.to_dataframe()
+        shape = structure.macro.data_vars[field].macro.variable.macro.shape
+        return len(shape) == 1
+
+    def add_seq_num(dataset):
+        """
+        Add a seq_num collumn to the dataset.
+        This also converts the dataset to a dataframe.
+
+        We need a seq_num column, which the server does not include, so we
+        do export on the client side.
+        """
+
+        df = dataset.to_dataframe()
         df2 = df.reset_index()  # Promote 'time' from index to column.
         df2.index.name = "seq_num"
         df3 = df2.reset_index()  # Promote 'seq_num' from index to column.
         df3["seq_num"] += 1  # seq_num starts at 1
-        df3.to_csv(
+        return df3
+
+    for stream_name, stream in run.items():
+
+        # Figure out the directory to write to.
+        scan_directory = f"{start['scan_id']}" if stream_name != "primary" else "."
+        directory = base_directory / scan_directory
+
+        # Prepare the data.
+        dataset = stream["data"]
+        structure = dataset.structure()
+        scalar_fields = {field for field in dataset if is_scalar(structure, field)}
+        ds = dataset.read(variables=scalar_fields)
+        dataframe = add_seq_num(ds)
+
+        # Write the data.
+        dataframe.to_csv(
             directory / f"{start['scan_id']}-{start['sample_name']}-{stream_name}.csv",
             index=False,
         )
 
 
 @task
-def json_export(ref):
+def json_export(raw_ref):
 
     """
     Export start document into a json file.
 
     Parameters
     ----------
-    ref: string
+    raw_ref: string
         Reference to a BlueskyRun. Can be a full uid, a partial uid,
         a scan id, or an index (e.g. -1).
 
     """
-    start = tiled_client_raw[ref].start
+    start = tiled_client_raw[raw_ref].start
     directory = EXPORT_PATH / "auto" / start["project_name"] / f"{start['scan_id']}"
     directory.mkdir(parents=True, exist_ok=True)
 
@@ -209,8 +235,8 @@ def json_export(ref):
 # Make the Prefect Flow.
 # A separate command is needed to register it with the Prefect server.
 with Flow("export") as flow:
-    ref = Parameter("ref")
-    processed_uids = write_dark_subtraction(ref)
-    tiff_export(ref, processed_uids)
-    csv_export(ref)
-    json_export(ref)
+    raw_ref = Parameter("ref")
+    processed_refs = write_dark_subtraction(raw_ref)
+    tiff_export(raw_ref, processed_refs)
+    csv_export(raw_ref)
+    json_export(raw_ref)
